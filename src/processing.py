b3b2bceac83fff15e3d3d6b7cfd5cdb1c2130e28
Andrey Cheltyomov <master.haunt@gmail.com>
2024-07-17 20:26:28 +0700

    processing.transactions_by_categories

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import re
+
 
 def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает на вход список словарей с информацией о транзакциях и возвращает список словарей с информацией
@@ -25,8 +27,28 @@ def sort_by_date(transactions: list[dict], *, descending: bool = True) -> list[d
 
 
 def search_in_transactions(transactions: list[dict], query: str) -> list[dict]:
+    """Функция принимает на вход список словарей с информацией о транзакциях и текстовую строку для поиска, а возвращает
+    список словарей с информацией о тех транзакциях, в описании (поле 'description') которых присутствует текст из
+    строки для поиска
+    """
+
     queried_transactions = []
     for transaction in transactions:
         if re.search(query, transaction["description"], flags=re.IGNORECASE):
             queried_transactions.append(transaction)
     return queried_transactions
+
+
+def transactions_by_categories(transactions: list[dict], categories: list[str]) -> dict:
+    """Функция принимает на вход список словарей с информацией о транзакциях и список с названиями категорий операций,
+     а возвращает словарь, в котором ключи - наименования категорий операций, а значения - количества операций
+     соответствующих категорий
+    """
+
+    result_dict = defaultdict(int)
+    for category in categories:
+        result_dict[category] = 0
+        for transaction in transactions:
+            if transaction["description"] == category:
+                result_dict[category] += 1
+    return result_dict
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -70,6 +70,7 @@ def test_sort_transactions_by_date_ascending(transactions_list):
 
 
 def test_search_in_transactions(adv_transactions_list):
+    """ Тестирование работы функции поиска текста в описании транзакции"""
     assert processing.search_in_transactions(adv_transactions_list, 'с карты') == [
         {
             "id": 895315941,
@@ -86,28 +87,36 @@ def test_search_in_transactions(adv_transactions_list):
 
     ]
     assert processing.search_in_transactions(adv_transactions_list, 'со счета') == [
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {"name": "USD", "code": "USD"},
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
         },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {
-            "amount": "43318.34",
-            "currency": {"name": "руб.", "code": "RUB"},
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    }]
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        }]
     assert processing.search_in_transactions(adv_transactions_list, 'конвертация') == []
+
+
+def test_transactions_by_categories(adv_transactions_list):
+    """Тестирование работы функции подсчёта количества транзакций по заданным категориям"""
+    assert processing.transactions_by_categories(adv_transactions_list,
+                                                 ["Перевод со счета на счет", "Перевод организации",
+                                                  "Погашение кредита"]) == {
+               "Перевод организации": 2, "Перевод со счета на счет": 2, "Погашение кредита": 0}
